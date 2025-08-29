16e5f425b480321f439c4654488ca527aa0e67da
Grv <grv08singh@gmail.com>
2025-08-29 21:40:14 +0530

    auto push

--- 05_python_proj/Python - Everything.py
+++ 05_python_proj/Python - Everything.py
@@ -177,9 +177,9 @@ for col in df.columns:
 # IMPORT LIBRARY
 import statistics as st                                                               #for descriptive statistics - in built in python
 from scipy import stats                                                               #for descriptive & inferential statistics
-#       
+
 # arr1, arr2 are lists of data
-#       
+
 st.mean(arr1)       
 st.median(arr1)     
 st.mode(arr1)       
@@ -195,98 +195,119 @@ st.linear_regression(arr1, list_2)
 
 st.geometric_mean(arr1)
 st.harmonic_mean(arr1)
-#
-#
-#
-#
-#
-#
-# 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###############################################################################################################
 #### Z-Test, Z Test
 ###############################################################################################################
-#
+
 z_stat = (st.mean(arr1) - pop_mean) / (pop_std/math.sqrt(N))                          #N = population size
 p_val = stats.norm.cdf(z_stat)                                                        #probability to the left of z_stat
-# 
+
 # OR
-# 
+
 from statsmodels.stats.weightstats import ztest                                       #one sampled, z test, z-test
 z_stat, p_val = ztest(x1=arr1, value = pop_mean, alternative='two-sided')             #for H1: arr1.mean != pop_mean
 z_stat, p_val = ztest(x1=arr1, value = pop_mean, alternative='larger')                #for H1: arr1.mean > pop_mean
-#
-#
-#
-#
+
+
+
+
 z_stat, p_val = ztest(x1=arr1,x2=list_2, value=pop_mean_diff, alternative='larger')   #two sample difference, z test, z-test
+
 # OR
+
 z_stat = ((mean(arr1)-mean(list_2)) - pop_mean_diff)/(s1_std**2/n1 + s2_std**2/n2)    #N = number of sample data-points
-# 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+ 
+
+
+
+
+
+
+
+
+
+
 ###############################################################################################################
 #### Proportion Z-Test, Z Test
 ###############################################################################################################
-# 
-# 
+
+
 from statsmodels.stats.proportion import proportions_ztest                            #z-test for proportion
 
 z_stat, p_val = proportions_ztest(count=arr1_count, nobs=total_pop, value=0.50, alternative="two-sided")
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-#
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+
 ###############################################################################################################
 #### T-Test, T Test
 ###############################################################################################################
-# 
+
 t_stat = (st.mean(arr1) - pop_mean) / (sample_std/math.sqrt(n-1))                     #n = number of sample data-points
 p_val = stats.t.cdf(t_stat, df=(n-1))                                                 #area to the left of t_stat
 p_val = stats.t.sf(t_stat, df=(n-1))                                                  #area to the left of t_stat - survival fqn (more accurate)
-#
+
 # OR
-#
+
 t_stat, p_val = stats.ttest_1samp(a=arr1, pop_mean)                                   #one sampled T-Test
-#
-#
+
+
 t_stat = (s1_mean - s2_mean) / (s1_std**2/n1 + s2_std**2/n2)                          #n1, n2 = number of sample data-points in s1, s2
-# 
+
 # OR
-# 
+
 t_stat, p_val = stats.ttest_ind(arr1, arr2)                                           #two independent sampled T-Test
-#
-# 
-# 
-# 
-# 
+
+
+
+
+
+ 
+
+
+ 
+
+
+ 
+
+
+ 
 ###############################################################################################################
 #### Chi2-Test, Chi2 Test
 ###############################################################################################################
@@ -295,38 +316,38 @@ from scipy.stats import chi2
 
 chi2_stat = sum((obs_arr - exp_arr)**2 / exp_arr)                                     #(observed - expected) / expected
 p_val = chi2.cdf(chi2_stat, df)                                                       #df = n-1, p_val is prob to the left of chi2_stat
-# 
+
 # OR
-# 
+
 from scipy.stats import chi2_contingency
 contingency_table = pd.crosstab(df['obs_arr1'], df['obs_arr2'])                       #two observed categorical variables
 chi2_stat, p_val, df, exp_frequencies = chi2_contingency(contingency_table)
-# 
+
 # OR
-# 
+
 from scipy.stats import chisquare
 chi2_stat, p_val = chisquare(f_obs = obs_arr, f_exp = exp_arr)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 ###############################################################################################################
 #### F-Test, F Test (ANOVA)
 ###############################################################################################################
-# 
+
 f_stat = max_var/min_var                                                              #ratio of two chi-square fqn, variance is chi-square
 p_val = stats.f.cdf(f_stat, df_of_max_var, df_of_min_var)                             #p_val to the left of f_stat, for one_tailed f_stat
-# 
+
 # OR
-# 
+
 from scipy.stats import f_oneway                                                      #one way anova
 f_stat, p_val = f_oneway(arr1, arr2, arr3)                                            #one way anova
-#
-#
-#
+
+
+
 
 
 
