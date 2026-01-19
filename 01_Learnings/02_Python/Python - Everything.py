1490b3fd2040e08b3d7a895e06db7b285f24d3b7
grv08singh <grv08singh@gmail.com>
2026-01-19 17:17:59 +0530

    auto_push

--- 01_Learnings/02_Python/Python - Everything.py
+++ 01_Learnings/02_Python/Python - Everything.py
@@ -3784,7 +3784,7 @@ plt.show()
 import tensorflow as tf
 from tensorflow import keras
 from keras import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout
 from keras.optimizers import Adam
 from keras.regularizers import l2
 
@@ -3861,6 +3861,58 @@ history = model.fit(X_train_normalized,
 
 
 
+###############################
+#creating CNN model : method
+###############################
+train_ds = keras.utils.image_dataset_from_directory(
+    directory = '/kaggle/input/dogsvscats/train',
+    labels = 'inferred',
+    label_mode = 'int',
+    batch_size = 32,
+    image_size = (256, 256)
+)
+
+validation_ds = keras.utils.image_dataset_from_directory(
+    directory = '/kaggle/input/dogsvscats/test',
+    labels = 'inferred',
+    label_mode = 'int',
+    batch_size = 32,
+    image_size = (256, 256)
+)
+
+def process(image, label):
+    imgage = tf.cast(image/255. , tf.float32)
+    return image, label
+train_ds = train_ds.map(process)
+validation_ds = validation_ds.map(process)
+
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size = (3,3), padding = 'valid', activation = 'relu', input_shape = (256, 256, 3)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2,2), strides = 2, padding = 'valid'))
+
+model.add(Conv2D(64, kernel_size = (3,3), padding = 'valid', activation = 'relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2,2), strides = 2, padding = 'valid'))
+
+model.add(Conv2D(128, kernel_size = (3,3), padding = 'valid', activation = 'relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2,2), strides = 2, padding = 'valid'))
+
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(.01))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(.01))
+model.add(Dense(1, activation = 'sigmoid'))
+
+model.summary()
+
+
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+history = model.fit(train_ds, epochs = 10, validation_data = validation_ds)
 
 
 
