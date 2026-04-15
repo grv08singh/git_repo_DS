f04f7a111980b04d49488a4720798a404fc54529
grv08singh <grv08singh@gmail.com>
2026-04-15 17:22:04 +0530

    01_masterRepo_auto_push

--- git_push - F_Drive.bat
+++ git_push - F_Drive.bat
@@ -2,49 +2,49 @@
 
 F:
 
-cd "F:\05 GIT\01_masterRepo"
+cd "F:\Grv\Grv\01 GIT\01_masterRepo"
 git pull
 git add .
 git commit -m "01_masterRepo_auto_push"
 git push
 
 
-cd "F:\05 GIT\14_Proj_ML_99Acres"
+cd "F:\Grv\Grv\01 GIT\14_Proj_ML_99Acres"
 git pull
 git add .
 git commit -m "14_Proj_ML_99Acres_auto_push"
 git push
 
 
-cd "F:\05 GIT\13_Misc_projects"
+cd "F:\Grv\Grv\01 GIT\13_Misc_projects"
 git pull
 git add .
 git commit -m "13_Misc_projects_auto_push"
 git push
 
 
-cd "F:\05 GIT\12_GenAI_projects"
+cd "F:\Grv\Grv\01 GIT\12_GenAI_projects"
 git pull
 git add .
 git commit -m "12_GenAI_projects_auto_push"
 git push
 
 
-cd "F:\05 GIT\11_NLP_projects"
+cd "F:\Grv\Grv\01 GIT\11_NLP_projects"
 git pull
 git add .
 git commit -m "11_NLP_projects_auto_push"
 git push
 
 
-cd "F:\05 GIT\10_DL_projects"
+cd "F:\Grv\Grv\01 GIT\10_DL_projects"
 git pull
 git add .
 git commit -m "10_DL_projects_auto_push"
 git push
 
 
-cd "F:\05 GIT\09_ML_projects"
+cd "F:\Grv\Grv\01 GIT\09_ML_projects"
 git pull
 git add .
 git commit -m "09_ML_projects_auto_push"
